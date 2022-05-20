c8c0a934e72981f7ed19ffc6aa9395fe7f7cb871
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-20 13:37:59 +0530

    [Zeeshan] : Added main page

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOPS_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\tObject Oriented Programming Problems");
+            Console.WriteLine("\t\t-------------------------------------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
